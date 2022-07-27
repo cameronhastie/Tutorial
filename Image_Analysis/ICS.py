194740f67c2f5890970a06298e490445445ccfab
cameronhastie <54183335+cameronhastie@users.noreply.github.com>
2022-07-27 14:47:18 -0400

    Implemented spiral shifting feature

--- Image_Analysis/ICS.py
+++ Image_Analysis/ICS.py
@@ -13,25 +13,35 @@ import numpy as np                  # This library is essential for manipulating
 import matplotlib.pyplot as plt     # This library is essential for plotting data.
 from matplotlib import animation
 
-# test_file = "C:\Users\Cameron\Documents\GitHub\Tutorial\Image_Analysis\data\2022-07-PDMS+TIRF\UnTIRF+BKGD.tif"
+#test_file = "C:/Users/Cameron/Documents/GitHub/Tutorial/Image_Analysis/data/2022-07-PDMS+TIRF/UnTIRF+BKGD.tif"
+#reff_file = "C:/Users/Cameron/Documents/GitHub/Tutorial/Image_Analysis/data/2022-07-PDMS+TIRF/TIRF+BKGD.tif"
 test_file = "C:/Users/Cameron/Documents/GitHub/Tutorial/Image_Analysis/data/2022-07-PDMS+TIRF/TIRF.tif"
-reff_file = "C:/Users/Cameron/Documents/GitHub/Tutorial/Image_Analysis/data/2022-07-PDMS+TIRF/unTIRF.tif"
+reff_file = "C:/Users/Cameron/Documents/GitHub/Tutorial/Image_Analysis/data/2022-07-PDMS+TIRF/TIRF.tif"
 # Grab the test and reference images
 test_image = plt.imread(test_file)
+test_image = np.log(test_image)
 test_image = (test_image - np.min(test_image)) / (np.max(test_image) - np.min(test_image))
 reff_image = plt.imread(reff_file)
+reff_image = np.log(reff_image)
 reff_image = (reff_image - np.min(reff_image)) / (np.max(reff_image) - np.min(reff_image))
 # Define fraction of image to be cropped
-crop_frac = 1/4 
+crop_frac = 1/10
 # Define the order of spiral
-dirs = ['R', 'U', 'L', 'D']
+directions = ['R', 'U', 'L', 'D']
 image_dim = np.size(reff_image[0,:])
-low_lim = int(np.round(image_dim*crop_frac))
-upp_lim = int(np.round(image_dim*(1-crop_frac)))
-shifts = np.floor(np.sqrt(2)*low_lim)
-shifts = int(shifts)
-diff_images = np.zeros([shifts, upp_lim-low_lim, upp_lim-low_lim])
+low_lim = int(np.floor(image_dim*crop_frac))
+upp_lim = int(np.floor(image_dim*(1-crop_frac)))
+shifts = int(low_lim)
+tran_images = np.zeros([shifts, upp_lim-low_lim, upp_lim-low_lim])
+direction_reps = np.zeros([2*shifts,1])
+for i in range(shifts):
+    loop_shifted_ind = 2*i
+    direction_reps[loop_shifted_ind] = i+1
+    direction_reps[loop_shifted_ind+1] = i+1
 L2_norm = np.zeros([shifts, 1])
+
+dir_counter = 0
+step_counter = 0
 
 # Initialization function: plot the background of each frame
 def init():
@@ -40,15 +50,41 @@ def init():
 
 # animation function.  This is called sequentially
 def animate(j):
-    low_lim_shift = low_lim - j
-    upp_lim_shift = upp_lim - j
-    shift_image = test_image[low_lim_shift:upp_lim_shift, low_lim_shift:upp_lim_shift]
-    diff_image = crop_image*shift_image
-    diff_images[j,:,:] = diff_image
-    L2_norm[j] = np.sum(diff_image)
-    test_movie.set_data(diff_image)
-    ax.set_title(["frame: ", str(j)])
+    # directions = [R,U,L,D]    |    direction_repeats = [1,1,3,3,4,4,5,5,...,Nshift,Nshift]
+    direct = directions[np.mod(j,len(directions)-1)]
+    shift_image = shift_crop_dir(direct,j)
+    tran_image = crop_image*shift_image
+    L2_norm[j] = np.sum(crop_image*shift_image)
+    #L2_norm[j] = np.sum(crop_image*shift_image)/np.sum(crop_image*crop_image)
+    #tran_image = np.power((crop_image-shift_image),2)
+    #L2_norm[j] = np.sqrt(np.sum(np.power((crop_image-shift_image),2)))
+    tran_images[j,:,:] = tran_image
+    test_movie.set_data(tran_image)
+    #test_movie.text("frame: " + str(j))
     return test_movie,
+
+def shift_crop_dir(x,j):
+    match x:
+        case "R":
+            low_lim_shift = low_lim + j
+            upp_lim_shift = upp_lim + j
+            shifted_image = test_image[low_lim_shift:upp_lim_shift, low_lim:upp_lim]
+            return shifted_image
+        case "U":
+            low_lim_shift = low_lim - j
+            upp_lim_shift = upp_lim - j
+            shifted_image = test_image[low_lim:upp_lim, low_lim_shift:upp_lim_shift]
+            return shifted_image
+        case "L":
+            low_lim_shift = low_lim - j
+            upp_lim_shift = upp_lim - j
+            shifted_image = test_image[low_lim_shift:upp_lim_shift, low_lim:upp_lim]
+            return shifted_image
+        case "D":
+            low_lim_shift = low_lim + j
+            upp_lim_shift = upp_lim + j
+            shifted_image = test_image[low_lim:upp_lim, low_lim_shift:upp_lim_shift]
+            return shifted_image
 
 # First set up the figure, the axis, and the plot element we want to animate
 fig = plt.figure()
@@ -58,7 +94,7 @@ test_movie = ax.imshow(crop_image, cmap='gray')
 # call the animator.  blit=True means only re-draw the parts that have changed.
 anim = animation.FuncAnimation(fig, animate, init_func=init,
                             frames=shifts, interval=20, blit=True) 
-#anim.save('basic_animation.gif', fps=5)
+anim.save('basic_animation.gif', fps=5)
 
 # save the animation as an mp4.  This requires ffmpeg or mencoder to be
 # installed.  The extra_args ensure that the x264 codec is used, so that
